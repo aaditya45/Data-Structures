bee53dae1080e4b0a4418d79adf2c66c58cf9ae7
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-06-09 08:57:13 +0530

    Time: 31 ms (9.55%), Space: 15.6 MB (75.27%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> res(2,0);
-        for(int i=0;i<numbers.size()-1;i++){
-            int a=i+1;
-            int b=numbers.size()-1;
-            int mid=0;
-            int curr_target=target-numbers[i];
-            
-            res[0]=i+1;
-            while(a<=b){
-                mid=(a+b)>>1;
-                if(curr_target==numbers[mid]){
-                    res[1]=mid+1;
-                    return res;
-                }else if(numbers[mid]>curr_target){
-                    b=mid-1;
-                }else{
-                    a=mid+1;
-                }
+        int a=0,b=numbers.size()-1;
+        
+        while(a<b){
+            int sum=numbers[a]+numbers[b];
+            if(sum==target){
+                return {a+1,b+1};
+            }else if(sum<target){
+                a++;;
+            }else{
+                b--;
             }
         }
-        cout<<"binary fault"<<endl;
-        return res;
+        return {0,0};
     }
 };
