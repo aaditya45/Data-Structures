221aeb958bf84d7a457c68ef82b1b46412cdfbf1
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-06-17 13:56:22 +0530

    Time: 107 ms (72.14%), Space: 45 MB (72.86%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+
+
+var isValid=function(nums,threshold,mid){
+    var sum=0
+    
+    for(var i=0;i<nums.length;i++){
+        sum+=Math.ceil(nums[i]/mid);
+    }
+    if(sum>threshold) return false
+    else return true
+}
+var smallestDivisor = function(nums, threshold) {
+    var high=Math.max.apply(null,nums)
+    var low=1
+    
+    
+    while(low<=high){
+        var mid=(high+low)>>1
+        if(isValid(nums,threshold,mid)){
+            res=mid
+            high=mid-1
+        }else low=mid+1
+    }
+    return res
+};
