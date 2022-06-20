8e08bcfcfc9fcb76d0a903d6ba384cf549e7f737
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-06-20 16:03:17 +0530

    Time: 5879 ms (5.08%), Space: 27.3 MB (72.41%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,34 +1,26 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        def slidingWindow(bloomDay,m,k,mid)->bool:
-            a=0
-            b=0
-            cnt=0
-            while b<len(bloomDay):
-                if bloomDay[b]<=mid:
-                    if (b-a)==k-1:
-                        cnt+=1
-                        b+=1
-                        a=b
-                    elif (b-a)>k-1:
-                        a+=1
-                    else:
-                        b+=1
-                else:
-                    b+=1
-                    a=b
-            if cnt>=m : return True
-            else : return False
-        
         if len(bloomDay)<m*k: return -1
-        
-        
         a=0
         b=max(bloomDay)
         res=-1
         while b>=a:
             mid=(a+b)>>1
-            if slidingWindow(bloomDay,m,k,mid):
+            x,y,cnt=0,0,0
+            while y<len(bloomDay):
+                if bloomDay[y]<=mid:
+                    if (y-x)==k-1:
+                        cnt+=1
+                        y+=1
+                        x=y
+                    elif (y-x)>k-1:
+                        x+=1
+                    else:
+                        y+=1
+                else:
+                    y+=1
+                    x=y
+            if cnt>=m:
                 res=mid
                 b=mid-1
             else:
