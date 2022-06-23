896a4db42de541a3ec8810dd7789830cdc97d13d
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-06-23 22:27:53 +0530

    Time: 42 ms (27.05%), Space: 14.4 MB (34.70%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[2001];
+    bool dfs(int curr,vector<vector<int>>&adj,vector<int>&vis){
+        if(vis[curr]) return false;
+        if(dp[curr]!=-1) return dp[curr];
+        vis[curr]=1;
+        for(auto num:adj[curr]){
+            if(!dfs(num,adj,vis)) return dp[curr]=false;
+        }
+        vis[curr]=0;
+        return dp[curr]=true;
+    }
+    bool canFinish(int n, vector<vector<int>>& p) {
+        memset(dp,-1,sizeof(dp));
+        vector<vector<int>> adj(n);
+        vector<int> vis(n,0);
+        for(auto num:p){
+            adj[num[0]].push_back(num[1]);
+        }
+        bool res=true;
+        for(int i=0;i<n;i++){
+            res=res&&dfs(i,adj,vis);
+        }
+        return res;
+        
+    }
+};
