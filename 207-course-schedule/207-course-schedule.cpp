0caa36798398bc2ed16f5dcba8af8ecf103f35b9
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-06-23 22:42:15 +0530

    Time: 30 ms (58.89%), Space: 14.6 MB (26.17%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int dp[2001];
+    //cycle detection
     bool dfs(int curr,vector<vector<int>>&adj,vector<int>&vis){
         if(vis[curr]==2) return false;
-        //if(dp[curr]!=-1) return dp[curr];
         vis[curr]=2;
         for(auto num:adj[curr]){
             if(vis[num]!=1&&!dfs(num,adj,vis)) return false;
@@ -12,7 +11,6 @@ public:
         return true;
     }
     bool canFinish(int n, vector<vector<int>>& p) {
-        //memset(dp,-1,sizeof(dp));
         vector<vector<int>> adj(n);
         vector<int> vis(n,0);
         for(auto num:p){
@@ -20,7 +18,6 @@ public:
         }
         bool res=true;
         for(int i=0;i<n;i++){
-            //if(dp[i]!=-1) res=res&&dp[i];
             res=res&&dfs(i,adj,vis);
         }
         return res;
