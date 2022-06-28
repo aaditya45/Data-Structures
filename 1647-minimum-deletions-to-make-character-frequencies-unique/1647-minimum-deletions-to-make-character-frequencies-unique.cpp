3985f712c9ba9cde6b9e0910eb54efa715316815
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-06-28 11:02:27 +0530

    Time: 128 ms (30.72%), Space: 17.2 MB (81.59%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int minDeletions(string s) {
-    int cnt[26] = {}, res = 0;
-    for (char ch : s)
-        ++cnt[ch - 'a'];
-    sort(begin(cnt), end(cnt));
-    for (int i = 24; i >= 0 && cnt[i] > 0; --i) {
-        res += max(0, cnt[i] - max(0, cnt[i + 1] - 1));
-        cnt[i] = min(cnt[i], max(0, cnt[i + 1] - 1));
+        vector<int> alp(26,0);
+        
+        for(int i=0;i<s.size();i++){
+            alp[s[i]-97]=alp[s[i]-97]+1;
+        }
+        sort(alp.begin(),alp.end());
+        /*for(auto x:alp){
+            cout<<x<<endl;
+        }*/
+        int res=0;
+        for(int i=0;i<alp.size()-1;i++){
+            if(alp[i]==0) continue;
+            int a=i,b=i+1;
+            while(a>=0&&alp[a]==alp[b]){
+                if(alp[a]==0) break;
+                alp[a]=alp[a]-1;
+                res++;
+                a--;
+                b--;
+            }
+            
+        }
+        return res;
+        
     }
-    return res;
-}
 };
