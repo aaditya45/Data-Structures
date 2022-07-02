472cd9a2f62d318f26ca2d8213ab52c07f99ab5c
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-07-02 10:35:05 +0530

    Time: 82 ms (19.79%), Space: 7.8 MB (29.41%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size()+1,vector<int>(p.size()+1,false));
+        dp[0][0]=true;
+        for(int i=1;i<=p.size();i++){
+            for(int j=0;j<=s.size();j++){
+                if(p[i-1]!='*'&&p[i-1]!='.'){
+                    dp[j][i]=j&&dp[j-1][i-1]&&(s[j-1]==p[i-1]);
+                }else if(p[i-1]=='*'){
+                    dp[j][i]=dp[j][i-2]||(j&&dp[j-1][i]&&(s[j-1]==p[i-2]||p[i-2]=='.'));
+                }else{
+                    dp[j][i]=j&&dp[j-1][i-1];
+                }
+            }
+        }
+        for(auto x:dp){
+            for(auto y:x){
+                cout<<y<<" ";
+            }
+            cout<<endl;
+        }
+        
+        return dp[s.size()][p.size()];
+    }
+};
