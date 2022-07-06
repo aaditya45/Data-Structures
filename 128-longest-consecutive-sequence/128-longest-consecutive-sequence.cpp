37e376c968352e716aceaabcc9c645f9406bd96f
Aaditya Sharma <75930326+aaditya45@users.noreply.github.com>
2022-07-06 10:33:01 +0530

    Time: 365 ms (44.01%), Space: 52.9 MB (9.16%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,23 +1,57 @@
 class Solution {
 public:
+    int parent[100001];
+    int size[100001];
+    
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);//path compression
+    }
+    
+    void union_set(int x,int y){
+        int rootX=find(x);
+        int rootY=find(y);
+        if(rootX!=rootY){
+            parent[rootX]=rootY;
+            size[rootY]+=size[rootX];
+        }
+    }
+    
+    
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(),nums.end()); // this is faster
-        int longest_sequence = 0;
-        for(int i=0; i<nums.size(); i++)
-        {
-            if(s.find(nums[i] - 1)!=s.end()) continue;
-            else
-            {
-                int count = 0;
-                int current_element = nums[i];
-                while(s.find(current_element) != s.end())
-                {
-                    count++;
-                    current_element++;
-                }
-                longest_sequence = max(longest_sequence,count);
+        
+        for(int i=0;i<nums.size();i++){
+            parent[i]=i;
+            size[i]=1;
+            
+        }
+        
+        unordered_map<int,int> mp;
+        
+        
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]!=0){
+                //cout<<nums[i]<<endl;
+                continue;
+            }
+            if(mp[nums[i]+1]!=0){
+                //cout<<nums[i]<<endl;
+                union_set(i,mp[nums[i]+1]-1);
+            }
+            if(mp[nums[i]-1]!=0){
+                union_set(i,mp[nums[i]-1]-1);
+            }
+            //cout<<nums[i]<<endl;
+            mp[nums[i]]=i+1;
+        }
+        
+        int res=0;
+        for(int i=0;i<nums.size();i++){
+            if(parent[i]==i){
+                res=max(res,size[i]);
             }
         }
-        return longest_sequence;
+        
+        return res;
     }
 };
